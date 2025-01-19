0d3509e3b92eebbb91fda7bd8cbe93d0fdb1c2bf
t4sk <tsk.nakamura@gmail.com>
2025-01-20 08:05:08 +0900

    flash

--- src/solutions/Flash.sol
+++ src/solutions/Flash.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import {IERC20} from "../interfaces/IERC20.sol";
+import {IPool} from "../interfaces/IPool.sol";
 import {POOL} from "../Constants.sol";
 
-contract Flash {}
+contract AaveV3FlashLoan {
+    IPool private constant pool = IPool(POOL);
+
+    function startFlashLoan(address token, uint256 amount) external {
+        pool.flashLoanSimple({
+            receiverAddress: address(this),
+            asset: token,
+            amount: amount,
+            params: abi.encode(msg.sender),
+            referralCode: 0
+        });
+    }
+
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 fee,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool) {
+        require(msg.sender == address(pool), "not authorized");
+        require(initiator == address(this), "invalid initiator");
+
+        address caller = abi.decode(params, (address));
+        IERC20(asset).transferFrom(caller, address(this), fee);
+
+        IERC20(asset).approve(msg.sender, amount + fee);
+
+        return true;
+    }
+}
