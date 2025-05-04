3140079c1efac58de08c78650f62af0f29cc6c67
t4sk <tsk.nakamura@gmail.com>
2025-05-04 11:46:33 +0900

    bank

--- notes/Bank.sol
+++ notes/Bank.sol
@@ -45,9 +45,6 @@ contract Bank {
 
     // R(n-1)
     uint256 public cumulativeRates = 1e18;
-    // R(k-1)
-    // user => cumulative rate when user borrowed
-    mapping(address => uint256) public userCumulativeRates;
 
     function calculateDebt(address user) external view returns (uint256) {
         return debts[user] * cumulativeRates / 1e18;
@@ -58,12 +55,10 @@ contract Bank {
     function borrow(uint256 amount) external {
         updateCumulativeRates();
         debts[msg.sender] += amount * 1e18 / cumulativeRates;
-        userCumulativeRates[msg.sender] = cumulativeRates;
     }
 
     function repay(uint256 amount) external {
         updateCumulativeRates();
         debts[msg.sender] -= amount * 1e18 / cumulativeRates;
-        userCumulativeRates[msg.sender] = cumulativeRates;
     }
 }
