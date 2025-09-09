dd3d5dc728238fcb5e174f9bb80fa363f7a51898
Zuhaib Mohammed <zzzuhaibmohd@gmail.com>
2025-09-09 20:02:49 +0530

    feat: aave flashloan() impl

--- foundry/src/exercises/Flash.sol
+++ foundry/src/exercises/Flash.sol
@@ -10,7 +10,15 @@ contract Flash {
     IPool public constant pool = IPool(POOL);
 
     // Task 1 - Initiate flash loan
-    function flash(address token, uint256 amount) public {}
+    function flash(address token, uint256 amount) public {
+        pool.flashLoanSimple({
+            receiverAddress: address(this),
+            asset: token,
+            amount: amount,
+            params: abi.encode(msg.sender),
+            referralCode: 0
+        });
+    }
 
     // Task 2 - Repay flash loan
     function executeOperation(
@@ -21,14 +29,16 @@ contract Flash {
         bytes calldata params
     ) public returns (bool) {
         // Task 2.1 - Check that msg.sender is the pool contract
-
+        require(msg.sender == address(pool), "not authorized");
         // Task 2.2 - Check that initiator is this contract
-
+        require(initiator == address(this), "invalid initiator");
         // Task 2.3 - Decode caller from params and transfer
         // flash loan fee from this caller
-
+        address caller = abi.decode(params, (address));
+        IERC20(asset).transferFrom(caller, address(this), fee);
         // Task 2.4 - Approve the pool to spend flash loaned amount + fee
-
+        IERC20(asset).approve(msg.sender, amount + fee);
         // Task 2.5 - Return true
+        return true;
     }
 }
